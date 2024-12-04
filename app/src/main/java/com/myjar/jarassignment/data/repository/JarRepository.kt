0d8e1853cb249617aaa237dde557a516cef29e97
Viraj76 <virajg.7680@gmail.com>
2024-12-04 10:45:54 +0530

    Getting API Response in Log

--- app/src/main/java/com/myjar/jarassignment/data/repository/JarRepository.kt
+++ app/src/main/java/com/myjar/jarassignment/data/repository/JarRepository.kt
@@ -2,7 +2,9 @@ package com.myjar.jarassignment.data.repository
 
 import com.myjar.jarassignment.data.api.ApiService
 import com.myjar.jarassignment.data.model.ComputerItem
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flow
 
 interface JarRepository {
@@ -12,7 +14,8 @@ interface JarRepository {
 class JarRepositoryImpl(
     private val apiService: ApiService
 ) : JarRepository {
-    override suspend fun fetchResults(): Flow<List<ComputerItem>> = flow {
-        apiService.fetchResults()
+    override suspend fun fetchResults(): Flow<List<ComputerItem>> = callbackFlow {
+        trySend(apiService.fetchResults())
+        awaitClose{}
     }
 }
--- app/src/main/java/com/myjar/jarassignment/ui/composables/AppNavigation.kt
+++ app/src/main/java/com/myjar/jarassignment/ui/composables/AppNavigation.kt
@@ -1,5 +1,6 @@
 package com.myjar.jarassignment.ui.composables
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
@@ -58,6 +60,12 @@ fun ItemListScreen(
     navController: NavHostController
 ) {
     val items = viewModel.listStringData.collectAsState()
+    LaunchedEffect(Unit) {
+        for(i  in items.value){
+            Log.d("Items", i.toString())
+
+        }
+    }
 
     if (navigate.value.isNotBlank()) {
         val currRoute = navController.currentDestination?.route.orEmpty()
